cedae1c4fb0940399aaa5e95ac48dea255019aec
aalaan.042008 <aalaan.042008@gmail.com>
2025-09-17 17:37:50 +0000

    💄ui(ProfileDetails): Crear los detalles principales del usuario - Añadir imagen de perfil. - Añadir nombre y estudiante en la pagina

--- components/profile-details.tsx
+++ components/profile-details.tsx
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
+import "../global.css";
+import { View,Image,Text} from 'react-native'
 import React from 'react'
 
 const ProfileDetails = () => {
   return (
-    <View>
-      <Text>profile-details</Text>
+    <View className='flex justify-center items-center'>
+      <Image 
+        source={require('../assets/images/icon.png')} 
+        className="max-w-36 max-h-36 rounded-full" 
+      />
+      <Text className="text-2xl font-bold">Alan Pulgarín</Text>
+      <Text className="text-xl text-gray-400">Estudiante</Text>
     </View>
   )
 }
